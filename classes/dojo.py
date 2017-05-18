058ce8fa92e8fd870e9dd93cd9e58d441dc84ac6
Akash <akashbaga011@gmail.com>
2017-05-19 01:07:53 +0300

    dojo class edited to comply with pep8

--- classes/dojo.py
+++ classes/dojo.py
@@ -1,173 +1,166 @@
 import random
 from room import Room, Office, Living_spaces
-from person import Person, Fellow,Staff
+from person import Person, Fellow, Staff
+
+
 class Dojo(object):
 
-	def __init__(self):
-		self.offices = []					#list of all create offices
-		self.living_spaces = [] 			#list of all created living
-		self.unoccupied_living = []
-		self.allocations_offices = {}		#offices with no. of occupants
-		self.allocations_living = {}		#living with no. occupants
-		self.allocated_office = {}			#stores allocated people with coreesponding room			
-		self.allocated_living = {}			#stores allocated people with coreesponding room
-		self.all_people = []
-		self.unallocated = []
+    def __init__(self):
+        self.offices = []  # list of all create offices
+        self.living_spaces = []  # list of all created living
+        self.unoccupied_living = []
+        self.allocations_offices = {}  # offices with no. of occupants
+        self.allocations_living = {}  # living with no. occupants
+        self.allocated_office = {}  # stores allocated people with coreesponding room
+        self.allocated_living = {}  # stores allocated people with coreesponding room
+        self.all_people = []
+        self.unallocated = []
 
-	def create_room(self, room_type, room_list):
-		
-		if room_type == 'office':
-			for each in room_list:
-				if each not in self.offices and each not in self.living_spaces:
-					new_room = Office(each)
-					self.offices.append(each)
-					print ("An Office called", each , "has been created")
-			
-				else:
-					print ("Room name",each ,"already in use")
-		else:
-			for each in room_list:
-				if each not in self.offices and each not in self.living_spaces:
-					new_room = Living_spaces(each)
-					self.living_spaces.append(each)
-					print ("A Living Space called", each , "has been created")
-				else:
-					print ("Room name",each ,"already in use")			
+    def create_room(self, room_type, room_list):
 
-	def update_room(self):
+        if room_type == 'office':
+            for each in room_list:
+                if each not in self.offices and each not in self.living_spaces:
+                    new_room = Office(each)
+                    self.offices.append(each)
+                    print("An Office called", each, "has been created")
 
-		for room in self.offices:
-			if room not in self.allocations_offices:
-				self.allocations_offices.update({room : 0})
+                else:
+                    print("Room name", each, "already in use")
+        else:
+            for each in room_list:
+                if each not in self.offices and each not in self.living_spaces:
+                    new_room = Living_spaces(each)
+                    self.living_spaces.append(each)
+                    print("A Living Space called", each, "has been created")
+                else:
+                    print("Room name", each, "already in use")
 
-		
+    def update_room(self):
 
-		for room in self.living_spaces:
-			if room not in self.allocations_living:
-				self.allocations_living.update({room : 0})
+        for room in self.offices:
+            if room not in self.allocations_offices:
+                self.allocations_offices.update({room: 0})
 
+        for room in self.living_spaces:
+            if room not in self.allocations_living:
+                self.allocations_living.update({room: 0})
 
+    def allocate_office(self, person_name):
 
-	def allocate_office(self, person_name):
+        self.update_room()
+        offices_to_let = []
 
-		self.update_room()
-		offices_to_let = []
+        for each_room in self.allocations_offices.keys():
+            if self.allocations_offices[each_room] < 7:
+                offices_to_let.append(each_room)
 
-		for each_room in self.allocations_offices.keys():
-			if self.allocations_offices[each_room] < 7:	
-				offices_to_let.append(each_room)
+        if len(offices_to_let) > 0:
 
+            allocate = random.choice(offices_to_let)
+            self.allocated_office.update({person_name: allocate})
+            self.allocations_offices[
+                allocate] = self.allocations_offices[allocate] + 1
+            print("An office", allocate, "has been allocated to", person_name,)
 
-		if len(offices_to_let) > 0:
+        else:
+            self.unallocated.append(person_name)
 
-			allocate = random.choice(offices_to_let)
-			self.allocated_office.update({person_name : allocate})
-			self.allocations_offices[allocate] = self.allocations_offices[allocate] + 1
-			print ("An office", allocate, "has been allocated to", person_name,)
-			
+    def allocate_living(self, person_name):
 
-		else:
-			self.unallocated.append(person_name)
+        self.update_room()
+        living_to_let = []
 
-	def allocate_living(self,person_name):
+        for each_room in self.allocations_living.keys():
+            if self.allocations_living[each_room] < 5:
+                living_to_let.append(each_room)
 
-		self.update_room()
-		living_to_let = []
+        if len(living_to_let) > 0:
 
-		for each_room in self.allocations_living.keys():
-			if self.allocations_living[each_room] < 5:
-				living_to_let.append(each_room)
+            allocate = random.choice(living_to_let)
+            self.allocated_living.update({person_name: allocate})
+            self.allocations_living[
+                allocate] = self.allocations_living[allocate] + 1
+            print("A living space ", allocate,
+                  "has been allocated to ", person_name,)
 
+        else:
+            self.unallocated.append(person_name)
 
-		if len(living_to_let) > 0 :
+    def add_person(self, person_name, position, accomodation):
 
-			allocate = random.choice(living_to_let)
-			self.allocated_living.update({person_name : allocate})
-			self.allocations_living[allocate] = self.allocations_living[allocate] + 1
-			print ("A living space ",allocate ,"has been allocated to ",person_name,) 
-			
+        if person_name not in self.all_people:
+            self.all_people.append(person_name)
+            print(position, person_name, "has been succesfully added !")
+        else:
+            print(person_name, "has already been added")
 
-		else:
-			self.unallocated.append(person_name)
+        if position == 'fellow':
+            new_person = Fellow(person_name)
+            if accomodation == 'Y':
+                self.allocate_office(person_name)
+                self.allocate_living(person_name)
+            else:
+                self.allocate_office(person_name)
+        else:
+            new_person = Staff(person_name)
+            self.allocate_office(person_name)
+            if accomodation == 'Y':
+                print("Staff cannot be allocated a living space")
 
-	def add_person(self,person_name,position,accomodation):
+    def print_room(self, room_name):
+        room_print = []
+        if room_name in self.allocated_office.values():
+            print(list(self.allocated_office.keys()))
 
-		if person_name not in self.all_people:
-			self.all_people.append(person_name)
-			print (position,person_name,"has been succesfully added !")
-		else:
-			print (person_name,"has already been added")
-					
-		if position == 'fellow':
-			new_person = Fellow(person_name)
-			if accomodation == 'Y':
-				self.allocate_office(person_name)
-				self.allocate_living(person_name)
-			else:
-				self.allocate_office(person_name)
-		else:
-			new_person = Staff(person_name)
-			self.allocate_office(person_name)
-			if accomodation == 'Y':
-				print ("Staff cannot be allocated a living space")			
+    def print_allocations(self):
 
+        for key in self.allocated_office.keys():
+            name = key
+            room = self.allocated_office[key]
+            print(room, name)
 
-	def print_room(self,room_name):
-		room_print =[]
-		if room_name in self.allocated_office.values():
-			print(list(self.allocated_office.keys()))
+        for key in self.allocated_living.keys():
+            name = key
+            room = self.allocated_living[key]
+            print(room, name)
 
+    def print_unallocated(self):
 
-				
+        print(self.unallocated)
 
+    def print_allocations_to_file(self, filename):
+        target = open(filename, 'w+')
+        target.truncate()
+        target.write("Room name\t\t")
+        target.write("Person\n")
+        for key in self.allocated_office.keys():
+            room = self.allocated_office[key]
+            name = key
+            target.write(room)
+            target.write("\t\t\t")
+            target.write(name)
+            target.write("\n")
+        for key in self.allocated_living.keys():
+            room = self.allocated_living[key]
+            name = key
+            target.write(room)
+            target.write("\t\t\t")
+            target.write(name)
+            target.write("\n")
 
-	def print_allocations(self):
-		
-		for key in self.allocated_office.keys():
-			name = key
-			room =self.allocated_office[key]
-			print (room, name)
+        target.close()
 
-		for key in self.allocated_living.keys():
-			name = key
-			room =self.allocated_living[key]
-			print (room, name)
+    def print_unallocations_to_file(self, filename):
+        target = open(filename, 'w+')
+        for name in self.unallocated:
+            target.write(name)
 
-
-	def print_unallocated(self):
-		
-		print (self.unallocated)
-
-	def print_allocations_to_file(self,filename):
-		target = open(filename , 'w+')
-		target.truncate()
-		target.write("Room name\t\t")
-		target.write("Person\n")
-		for key in self.allocated_office.keys():
-			room = self.allocated_office[key]
-			name = key
-			target.write(room)
-			target.write("\t\t\t")
-			target.write(name)
-			target.write("\n")
-		for key in self.allocated_living.keys():
-			room = self.allocated_living[key]
-			name = key
-			target.write(room)
-			target.write("\t\t\t")
-			target.write(name)
-			target.write("\n")
-		
-		target.close()	
-
-	def print_unallocations_to_file(self,filename):
-		target = open(filename , 'w+')
-		for name in self.unallocated:
-			target.write(name)
-
-		target.close()
+        target.close()
 
 
-
-
-
+new = Dojo()
+new.create_room('office',['London','x'])
+new.add_person('james sa','fellow','Y')
+new.print_allocations()
+new.print_unallocated()
